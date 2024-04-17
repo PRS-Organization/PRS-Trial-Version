1475ed0491c9e70316d955beb17a16cadeba3656
Zhuoqun Xu <2502047102@qq.com>
2024-04-17 19:16:50 +0800

    Update prs_demo.py

--- prs_demo.py
+++ prs_demo.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     # How many NPCs are there
     ma = np.array(prs.server.maps.floor3)
     # print(ma.shape)(172, 228)
-
-    prs.agent.goto_target_goal((2, 130, 120), position_mode=1)
+    prs.agent.rotate_right(degree=30)
+    # prs.agent.goto_target_goal((2, 130, 120), position_mode=1)
     # robot navigate to floor3 point (130, 120)
     # API document come soon!
     map_room = prs.server.maps.floor3
